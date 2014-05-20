3d6a6b8e83ac4ac70642318a4ffcab317d60e9d0
John Doty <jrhdoty@gmail.com>
2014-05-19 20:26:59 -0700

    implements Game class and basic game functionality

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -53,8 +53,8 @@ angular.module('nbackApp')
     //turn a random tile on
     game.Board.prototype.activateRandomTile = function(){
       var tile = this.getRandomTile();
-        tile.active = true;
-        return tile;
+      tile.active = true;
+      return tile;
     };
 
     //define sequence class
@@ -77,33 +77,49 @@ angular.module('nbackApp')
     };
 
     //define Game class
-    game.Game = function(board){
+    game.Game = function(board, n){
       this.board = board;
+      this.seq = new game.Sequence();
+      this.correctGuesses   = 0;
+      this.incorrectGuesses = 0;
+      this.missedGuesses    = 0;
+      this.n = n;
+      this.playerGuessed = false;
     };
 
-    game.Game.prototype.play = function(n, gameLength){
+    game.Game.prototype.play = function(gameLength){
       var self = this;
-      var seq = new game.Sequence();
-
+      console.log('gameLength is: ', gameLength);
+      var self = this;
 
       var nextRound = function(){
         self.board.reset();
-        seq.add(self.board.activateRandomTile());
-        console.log(seq.match(n));
+        self.seq.add(self.board.activateRandomTile());
       };
 
       $interval(function(){
-        nextRound();
-      }, 2000, n+gameLength);
+        //if match and no player guess input then increment missedGuesses count
+        if(self.seq.match(self.n) && self.playerGuessed === false){
+          self.missedGuesses++;
+        }
+        nextRound();       
+      }, 2000, this.n+gameLength);
+    };
 
+    game.Game.prototype.guess = function(){
+      if(this.seq.match(this.n)){
+        this.correctGuesses++;
+      } else {
+        this.incorrectGuesses++;
+      }
+      this.playerGuessed = true;
     };
 
     //create board
     game.board = new game.Board(3, 3);
-    var g = new game.Game(game.board);
-    g.play(2, 20);
+    $scope.nBack = new game.Game(game.board, 2);
+    // g.play(2, 20);
     // game.board.reset();    
-
 
     //for the game loop use $interval
     //angular native loop
