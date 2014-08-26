289804944d429d36c27e2c02b76b00b540c9ba5f
John Doty <jrhdoty@gmail.com>
2014-08-26 16:48:00 -0700

    update to inject stateprovider service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,68 +1,9 @@
 'use strict';
 
-angular.module('nbackApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider, $locationProvider, $httpProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/home',
-        controller: 'MainCtrl'
-      })
-      .when('/login', {
-        templateUrl: 'partials/login',
-        controller: 'LoginCtrl'
-      })
-      .when('/signup', {
-        templateUrl: 'partials/signup',
-        controller: 'SignupCtrl'
-      })
-      .when('/nback', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl',
-        authenticat: true
-      })
-      .when('/stats', {
-        templateUrl: 'partials/stats',
-        controller: 'StatsCtrl',
-        authenticate: true
-      })
-      .when('/settings', {
-        templateUrl: 'partials/settings',
-        controller: 'SettingsCtrl',
-        authenticate: true
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+angular.module('nbackApp', ['ui.router'])
+  .config(function($stateProvider){
+    $stateProvider
+    .state('index', {
       
-    $locationProvider.html5Mode(true);
-      
-    // Intercept 401s and redirect you to login
-    $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
-      return {
-        'responseError': function(response) {
-          if(response.status === 401) {
-            $location.path('/login');
-            return $q.reject(response);
-          }
-          else {
-            return $q.reject(response);
-          }
-        }
-      };
-    }]);
-  })
-  .run(function ($rootScope, $location, Auth) {
-
-    // Redirect to login if route requires auth and you're not logged in
-    $rootScope.$on('$routeChangeStart', function (event, next) {
-      
-      if (next.authenticate && !Auth.isLoggedIn()) {
-        $location.path('/login');
-      }
     });
   });
