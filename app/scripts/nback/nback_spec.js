6e5673eefd37425859b9462fdc112df90f895e72
John Doty <jrhdoty@gmail.com>
2014-08-27 15:09:03 -0700

    setup file for testing nback service

---
+++ app/scripts/nback/nback_spec.js
@@ -0,0 +1,13 @@
+// expect  = require('chai').expect;
+
+'use strict';
+
+describe('nback service', function(){
+  beforeEach(function(){});
+  
+  it('should run this test', function(){
+    console.log(expect);
+    expect(true).to.equal(true);
+  });
+});
+
